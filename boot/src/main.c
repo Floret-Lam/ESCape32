4f4ca44a17aaacb834aaba1a79359720312475c4
Floret-Lam <462078146@qq.com>
2025-07-17 14:33:15 +0800

    Update main.c

--- boot/src/main.c
+++ boot/src/main.c
@@ -20,7 +20,7 @@
 #define REVISION 3
 
 /* 引导版本号存储地址 */
-#define BOOT_VERSION_ADDR (_boot_version_start)
+#define BOOT_VERSION_ADDR 0x08001800
 __attribute__((section(".boot_version"))) 
 const uint32_t bootloader_version = REVISION;
 
